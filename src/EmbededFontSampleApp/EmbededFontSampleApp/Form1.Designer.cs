1a9a6ba86daaa022b297c58cdb846f16327eb04c
Pon Cheol, Ku <bbon72@idino.co.kr>
2017-09-01 16:01:02 +0900

    add project files

---
+++ src/EmbededFontSampleApp/EmbededFontSampleApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace EmbededFontSampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1234567890";
+            this.label2.UseCompatibleTextRendering = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 123);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ src/EmbededFontSampleApp/EmbededFontSampleApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmbededFontSampleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+            LoadFont();
+
+            comboBox1.SelectedIndexChanged += (s, e) =>
+            {
+                var cbx = (ComboBox)s;
+
+               var fontFamily= cbx.SelectedItem as FontFamily;
+
+                Font = new Font(fontFamily, Font.SizeInPoints);
+
+                Invalidate(true);
+
+                this.UpdateStyles();
+            };
+
+            label1.Text = sampleTextData1;
+
+            label2.Text = sampleTextData2;
+        }
+
+        private void LoadFont()
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+
+            string[] fontResourceNames =  {
+                "EmbededFontSampleApp.Fonts.D2Coding.ttf",
+                "EmbededFontSampleApp.Fonts.NanumGothic.ttf" };
+
+            foreach (var fontResourceName in fontResourceNames)
+            {
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fontResourceName))
+                {
+                    int streamLength = (int)stream.Length;
+
+                    IntPtr data = Marshal.AllocCoTaskMem(streamLength);
+
+                    byte[] fontData = new byte[streamLength];
+
+                    stream.Read(fontData, 0, streamLength);
+
+                    Marshal.Copy(fontData, 0, data, streamLength);
+
+                    pfc.AddMemoryFont(data, streamLength);
+
+                    stream.Close();
+
+                    Marshal.FreeCoTaskMem(data);
+                }
+            }
+
+            comboBox1.Items.AddRange(pfc.Families);
+
+            var fontFamliy = pfc.Families[0];
+
+            Font = new Font(fontFamliy.Name, Font.SizeInPoints);
+        }
+
+
+        private string sampleTextData1 = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+        private string sampleTextData2 = "1234567890";
+    }
+}
